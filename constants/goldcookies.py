5839f2c81535903f4e212d7f680969be058b4292
MiguelCRP <miguelcrpires@gmail.com>
2022-03-20 18:42:08 +0000

    Optimization & Added new Frenzy Type

--- constants/goldcookies.py
+++ constants/goldcookies.py
@@ -3,8 +3,18 @@ import constants.gcookietypes as types
 goldcookies = [
     {
         "type": types.CFRENZY,
-        "survival_time": 7, # secs
-        "effect_duration": 15, # secs
-        "effect_amount": 7# cps boost
+        "static_display_time": 2,  # secs
+        "fade_time": 6,  # secs
+        "effect_duration": 13,  # secs
+        "effect_amount": 777,  # cps boost
+        "effect_icon": "here goes the surface to be displayed",
+    },
+    {
+        "type": types.FRENZY,
+        "static_display_time": 2,  # secs
+        "fade_time": 6,  # secs
+        "effect_duration": 77,  # secs
+        "effect_amount": 7,  # cps boost
+        "effect_icon": "here goes the surface to be displayed",
     }
 ]
